75cc81340a0a9b1b9f038a6fc8ebbe5e7d6bc5b6
intohole <intoblack86@gmail.com>
2015-10-18 11:09:15 +0800

    1. 添加__main__变量使用方法wq

---
+++ test/test__main__filed.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+
+
+"""原因想写一个工具类 ， 直接调用python 函数， 通过函数名称 ， 直接调用类内方法
+    __main__    可以实现这个想法
+
+"""
+
+
+
+import __main__
+
+
+
+
+
+def p():
+    print 'test'
+
+
+
+
+
+
+getattr(__main__ , 'p')()
